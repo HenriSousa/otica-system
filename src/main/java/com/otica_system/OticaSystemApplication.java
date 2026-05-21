def1093ebc5d865def443a930edcaf2ea4d813b9
HenriSousa <henrisousa7@hotmail.com>
2026-05-20 21:54:01 -0300

    Initial commit

---
+++ src/main/java/com/otica_system/OticaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.otica_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OticaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OticaSystemApplication.class, args);
+	}
+
+}
